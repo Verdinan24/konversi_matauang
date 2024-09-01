401a5e22118d30a83cd343e75fd87beb24f355c8
Verdinan24 <156978741+Verdinan24@users.noreply.github.com>
2024-09-01 08:42:42 +0700

    Update konversi_mata_uang_asean.py

--- konversi_mata_uang_asean.py
+++ konversi_mata_uang_asean.py
@@ -1,11 +1,9 @@
+import requests
 import streamlit as st
-from forex_python.converter import CurrencyRates, CurrencyCodes
 
-c = CurrencyRates()
-code = CurrencyCodes()
+st.title("Aplikasi Konversi Mata Uang ASEAN dengan API")
 
-st.title("Aplikasi Konversi Mata Uang ASEAN")
-
+# Daftar mata uang negara-negara ASEAN
 asean_currencies = {
     'Indonesia (IDR)': 'IDR',
     'Malaysia (MYR)': 'MYR',
@@ -17,17 +15,13 @@ asean_currencies = {
     'Kamboja (KHR)': 'KHR',
     'Laos (LAK)': 'LAK',
     'Myanmar (MMK)': 'MMK',
-    'Timor Leste (USD)': 'USD'  
+    'Timor Leste (USD)': 'USD'
 }
 
-from_currency_label = st.selectbox(
-    'Pilih mata uang awal:',list(asean_currencies.keys())
-)
+from_currency_label = st.selectbox('Pilih mata uang awal:', list(asean_currencies.keys()))
 from_currency = asean_currencies[from_currency_label]
 
-to_currency_label = st.selectbox(
-    'Pilih mata uang tujuan:',list(asean_currencies.keys())
-)
+to_currency_label = st.selectbox('Pilih mata uang tujuan:', list(asean_currencies.keys()))
 to_currency = asean_currencies[to_currency_label]
 
 amount = st.number_input("Masukkan jumlah uang:", min_value=0.0, format="%.2f")
@@ -35,12 +29,19 @@ amount = st.number_input("Masukkan jumlah uang:", min_value=0.0, format="%.2f")
 if st.button("Konversi"):
     if from_currency != to_currency:
         try:
-            converted_amount = c.convert(from_currency, to_currency, amount)
-            symbol = code.get_symbol(to_currency)
+            api_key = '6b48d67e8ecbc41cab19a3b3'  # API key Anda
+            url = f"https://v6.exchangerate-api.com/v6/{api_key}/pair/{from_currency}/{to_currency}/{amount}"
 
-            st.success(f"{amount:.2f} {from_currency} = {symbol} {converted_amount:.2f} {to_currency}")
+            response = requests.get(url)
+            data = response.json()
+
+            if data['result'] == 'success':
+                converted_amount = data['conversion_result']
+                st.success(f"{amount:.2f} {from_currency} = {converted_amount:.2f} {to_currency}")
+            else:
+                st.error("Error dalam konversi mata uang")
+
         except Exception as e:
             st.error(f"Error: {e}")
     else:
         st.warning("Mata uang awal dan tujuan tidak boleh sama!")
-
